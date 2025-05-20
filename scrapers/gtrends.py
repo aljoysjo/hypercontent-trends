bf2b01634abdac67332e9a5a7670825a16fbecb5
aljoysjo <albertojaraujon@gmail.com>
2025-05-19 21:02:37 -0300

    Update gtrends.py

--- scrapers/gtrends.py
+++ scrapers/gtrends.py
@@ -1,1 +1,9 @@
+from pytrends.request import TrendReq
+import pandas as pd, datetime as dt
 
+py = TrendReq(hl="es-ES", tz=-180)
+py.build_payload(["*"], timeframe="now 7-d", geo="AR")
+df = py.trending_searches(pn="argentina")
+df["source"] = "gtrends"
+df["ts"] = dt.date.today()
+df.to_csv("data/gtrends.csv", index=False)
